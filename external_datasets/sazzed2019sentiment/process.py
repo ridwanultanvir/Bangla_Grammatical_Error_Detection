9acec14b41b1b478c8e396e5fb522d8766a75670
ShayekhBinIslam <shikh.bin.islam@gmail.com>
2023-02-22 13:00:40 +0600

    sent data bert large

--- external_datasets/sazzed2019sentiment/process.py
+++ external_datasets/sazzed2019sentiment/process.py
@@ -8,12 +8,12 @@ positive = "gt/all_positive_8500.txt"
 negative = "gt/all_negative_3307.txt"
 pos_out = "processed/all_p8500_n3307.csv"
 
-df_pos = pd.read_csv(positive, header=None, encoding="utf-8", sep="\n")
+df_pos = pd.read_table(positive, header=None, encoding="utf-8", sep='\r')
 df_pos.columns = ["text"]
-df_pos["label"] = "p"
-df_neg = pd.read_csv(negative, header=None, encoding="utf-8", sep="\n")
+# df_pos["label"] = "p"
+df_neg = pd.read_table(negative, header=None, encoding="utf-8", sep='\r')
 df_neg.columns = ["text"]
-df_neg["label"] = "n"
+# df_neg["label"] = "n"
 # Concatenate to df_all
 df_all = pd.concat([df_pos, df_neg], axis=0)
 
