50c6032090281c9eb5e2a5d4bf296165493cd639
ridwanultanvir <1705016@ugrad.cse.buet.ac.bd>
2023-02-28 12:48:42 +0600

    test2 pure map added

--- data/transiterate2/test2.py
+++ data/transiterate2/test2.py
@@ -37,12 +37,22 @@ phonetic_dict = {
     'ি': '◌ী',
     'ু': '◌ূ'
 }
-
+def pure_char(s):
+        ret=''
+        for c in s:
+            if ord(c) == 9676:
+                continue
+            ret+=c
+        return ret
 # Load the original CSV file with the matched sentences and tokens
 df_matched = pd.read_csv('test1.csv')
 
 # Replace the characters in the matched token based on the phonetic_dict
-df_matched['gt'] = df_matched.apply(lambda row: row['correct_sentence'].replace(row['matched_token'], ''.join([phonetic_dict.get(c, c) for c in row['matched_token']])), axis=1)
+df_matched['gt'] = df_matched.apply(lambda row: row['correct_sentence'].replace(row['matched_token'], ''.join([pure_char(phonetic_dict.get(c, c)) for c in row['matched_token']])), axis=1)
+# df_matched['gt'] = df_matched.apply(lambda row: row['correct_sentence'].replace(
+#     row['matched_token'], 
+#     ''.join([phonetic_dict.get(pure_char(c), c) for c in row['matched_token']])
+# ), axis=1)
 
 # Create a new CSV file with only the corrected sentences
 df_matched[['correct_sentence', 'gt','matched_token']].to_csv('test2.csv', index=False)
